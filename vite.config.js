149740eaad0fb0f8a8017c6619f50ef301d936d1
AyshaReeha <ayshareeha719@gmail.com>
2025-04-17 22:43:38 +0530

    Updated project with latest content

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const repoName = 'Portfolio'
+ // if your repo is named "Portfolio"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // if your repo is named "Portfolio"
+
 })
